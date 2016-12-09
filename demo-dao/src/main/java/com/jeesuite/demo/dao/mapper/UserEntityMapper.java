a26a6972f90dd4ecdeedb00922f9065e2976b339
vakinge <vakinge@gmail.com>
2016-12-09 22:47:15 +0800

    +

--- demo-dao/src/main/java/com/jeesuite/demo/dao/mapper/UserEntityMapper.java
+++ demo-dao/src/main/java/com/jeesuite/demo/dao/mapper/UserEntityMapper.java
@@ -3,6 +3,8 @@ package com.jeesuite.demo.dao.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.Select;
 
 import com.jeesuite.demo.dao.entity.UserEntity;
 import com.jeesuite.mybatis.plugin.cache.annotation.Cache;
@@ -14,8 +16,11 @@ public interface UserEntityMapper extends BaseMapper<UserEntity> {
 	@Cache
 	public UserEntity findByMobile(@Param("mobile") String mobile);
 	
-	@Cache(uniqueResult = false,keyPattern = "UserEntity.type:%s")
+	@Cache
 	List<String> findByType(@Param("type") int type);
 	
-
+	@Cache
+	@Select("SELECT count(*) FROM users where type=#{type}")
+	@ResultMap("BaseResultMap")
+    int countByType(@Param("type") int type);
 }
